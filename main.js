69b372b3c70a948eba123ca06bba68d8732459aa
BoonDoggle009 <164583648+BoonDoggle009@users.noreply.github.com>
2024-04-03 17:36:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-var b = {log: function(text){console.log(text)}, randomInt: function(first,second){var inT = Math.floor(Math.random() * second) + first; return inT},}
+var b = {log: function(text){console.log(text)}, randomInt: function(first,second){var inT = Math.floor(Math.random() * second) + first; return inT}, detectOverlapA: function(element1, element2) {
+                var el11 = document.getElementById(element1)
+                var el22 = document.getElementById(element2)
+                var el1 = el11.getBoundingClientRect()
+                var el2 = el22.getBoundingClientRect()
+                var colliding = 0
+                
+                if (el1.x >= el2.x - el2.width + 1 && el1.x <= el2.x + el2.width && el1.y >= el2.y - el2.width + 1 && el1.y <= el2.y + el2.width){
+                    console.log("collision")
+                    colliding = 1
+                }
+                return colliding
+            },
+        }
